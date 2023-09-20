baeb7b4c8eee4fab2ecb25246eca0b5d5bf037d4
Rodrigo Marques <rodrigomarques1993@gmail.com>
2023-09-20 14:52:48 -0300

    Add `AddSongToPlaylistChange` class

---
+++ lib/models/changes/add_song_to_playlist_change.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class AddSongToPlaylistChange < Change
+  def apply_to(output_data)
+    playlist = output_data['playlists'].find { |p| p['id'] == playlist_id }
+    playlist['song_ids'] << song_id
+  end
+
+  private
+
+  def playlist_id
+    @playlist_id ||= @data['playlist_id']
+  end
+
+  def song_id
+    @song_id ||= @data['song_id']
+  end
+end
